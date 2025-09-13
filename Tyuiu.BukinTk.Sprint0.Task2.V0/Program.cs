c3ed4dfe859506bdb2ec7373848827c488ffe60b
forengeit345 <timophey2907@gmail.com>
2025-09-13 13:05:55 +0500

    Task2

---
+++ Tyuiu.BukinTk.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.BukinTK.Sprint0.Task2.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Тимофей"));
+        Console.ReadKey();
+        Console.ReadKey();
+    }
+}
